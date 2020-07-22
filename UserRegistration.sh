fb1d58730988e8ef08c4059707c27f1544a7867d
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-22 02:56:01 -0700

    Added:FirstName Pattern

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "Welcome to User Registration problem"
+
+#validate first name
+echo "Enter the FirstName:"
+read name
+namePat="^[A-Z]{1}[a-z]{2,}$"
+if [[ $name =~ $namePat ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
